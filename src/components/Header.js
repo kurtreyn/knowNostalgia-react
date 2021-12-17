57eb8eae36e3a0cb3e08e8ea23134d01937fc53a
KurtReyn <kurtreyn@yahoo.com>
2021-12-17 13:18:01 -0500

    added mediaObject.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { mediaObject } from '../shared/MediaObject';
 
 export const Header = () => {
   return (
     <header>
       <div className="jumbotron-fluid">
         <div className="container-fluid hero-container">
-          <img src="./src/images/hero-img-lg.jpg" alt="hero image" />
+          <img src={mediaObject.image} alt={mediaObject.name} />
         </div>
       </div>
     </header>
---
+++ src/shared/MediaObject.js
@@ -0,0 +1,22 @@
+export const mediaObject = [
+  {
+    id: 0,
+    name: 'hero image',
+    image: './src/images/hero-img-lg.jpg',
+  },
+  {
+    id: 1,
+    name: 'Chrome River Campground ',
+    image: 'assets/images/chrome-river.jpg',
+  },
+  {
+    id: 2,
+    name: 'Breadcrumb Trail Campground',
+    image: 'assets/images/breadcrumb-trail.jpg',
+  },
+  {
+    id: 3,
+    name: 'Redux Woods Campground',
+    image: 'assets/images/redux-woods.jpg',
+  },
+];
