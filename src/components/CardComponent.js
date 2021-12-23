0eaec15b8f614964a2dab05ddd95e394b44f33f8
kurtreyn <kurtreyn@yahoo.com>
2021-12-23 00:48:21 -0500

    attempt to generate more cards

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,33 +1,49 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+} from 'reactstrap';
 import { HomePageContent } from '../shared/HomePageContent';
 
-class CardComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cardContent: HomePageContent,
-    };
-  }
+function RenderCard({ card }) {
+  return (
+    <Card className="h-100">
+      <CardImg variant="top" src={card.image} />
+      <div className="fact-container hide-item" id="semi-charmed">
+        <button type="button" className="close close-button" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <div className="fact-info"></div>
+      </div>
+      <CardBody className="d-flex flex-column">
+        <CardTitle>{card.category}</CardTitle>
+        <CardText>
+          Semi-Charmed Life by Third Eye Blind was released in 1997.
+        </CardText>
+        <Button variant="primary" className="btn-fun-fact">
+          Fun Fact
+        </Button>
+      </CardBody>
+    </Card>
+  );
+}
 
-  render() {
-    const cardCont = this.state.cardContent.map((content) => {
-      return (
-        <Card>
-          <CardImg top src={content.image} alt={content.title} />
-          <CardBody>
-            <CardTitle>{content.title}</CardTitle>
-            <CardText>{content.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    });
+function CardComponent() {
+  const cardComp = this.props.cards.map((card) => {
     return (
-      <div className="container">
-        <div className="row">{cardCont}</div>
+      <div
+        key={card.id}
+        className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"
+      >
+        <RenderCard card={card} />
       </div>
     );
-  }
+  });
+  return <div className="row">{cardComp}</div>;
 }
 
 export default CardComponent;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -11,40 +11,21 @@ import CardComponent from './CardComponent';
 import { HomePageContent } from '../shared/HomePageContent';
 import semiCharmed from '../images/thirdEyeBlind-semiCharmedLife.jpg';
 
-class App extends Component {
+class HomeComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cardContent: HomePageContent,
+      card: HomePageContent,
     };
   }
 
   render() {
     return (
-      <Card className="h-100">
-        <CardImg variant="top" src={semiCharmed} />
-        <div className="fact-container hide-item" id="semi-charmed">
-          <button
-            type="button"
-            className="close close-button"
-            aria-label="Close"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-          <div className="fact-info"></div>
-        </div>
-        <CardBody className="d-flex flex-column">
-          <CardTitle>Music</CardTitle>
-          <CardText>
-            Semi-Charmed Life by Third Eye Blind was released in 1997.
-          </CardText>
-          <Button variant="primary" className="btn-fun-fact">
-            Fun Fact
-          </Button>
-        </CardBody>
-      </Card>
+      <>
+        <CardComponent />
+      </>
     );
   }
 }
 
-export default App;
+export default HomeComponent;
