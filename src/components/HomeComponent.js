d2df730a9c9bdf6d3842a4e3ce73c029f01cc565
kurtreyn <kurtreyn@yahoo.com>
2021-12-22 21:02:21 -0500

    made working card on HomeComponent

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+} from 'reactstrap';
 import CardComponent from './CardComponent';
 import { HomePageContent } from '../shared/HomePageContent';
+import semiCharmed from '../images/thirdEyeBlind-semiCharmedLife.jpg';
 
 class App extends Component {
   constructor(props) {
@@ -12,21 +20,29 @@ class App extends Component {
   }
 
   render() {
-    const cardCont = this.state.cardContent.map((content) => {
-      return (
-        <Card>
-          <CardImg top src={content.image} alt={content.title} />
-          <CardBody>
-            <CardTitle>{content.title}</CardTitle>
-            <CardText>{content.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    });
     return (
-      <div className="container">
-        <div className="row">{cardCont}</div>
-      </div>
+      <Card className="h-100">
+        <CardImg variant="top" src={semiCharmed} />
+        <div className="fact-container hide-item" id="semi-charmed">
+          <button
+            type="button"
+            className="close close-button"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <div className="fact-info"></div>
+        </div>
+        <CardBody className="d-flex flex-column">
+          <CardTitle>Music</CardTitle>
+          <CardText>
+            Semi-Charmed Life by Third Eye Blind was released in 1997.
+          </CardText>
+          <Button variant="primary" className="btn-fun-fact">
+            Fun Fact
+          </Button>
+        </CardBody>
+      </Card>
     );
   }
 }
