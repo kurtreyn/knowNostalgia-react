c56c5826a63faca3e686dd345edad50ea1c09cd7
kurtreyn <kurtreyn@yahoo.com>
2021-12-28 03:40:01 -0500

    work on home functionality

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,21 +8,33 @@ import {
   Button,
 } from 'reactstrap';
 import { HomePageContent } from '../shared/HomePageContent';
-
+let currentBtn = '';
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cards: HomePageContent,
     };
+    this.displayFact = this.displayFact.bind(this);
   }
 
-  displayFact(event) {
-    let button = event.target;
-    let buttonID = button.dataset.button;
-    const container = document.querySelector('.fact-container');
-    console.log(`button ID: ${buttonID}`);
-    container.classList.remove('hide-item');
+  // displayFact(event) {
+  //   let button = event.target;
+  //   let buttonID = button.dataset.button;
+  //   const container = document.querySelector('.fact-container');
+  //   let containerID = container.dataset.container;
+  //   // console.log(containerID);
+  //   // console.log(`button ID: ${buttonID}`);
+  //   // container.classList.remove('hide-item');
+  // }
+
+  displayFact() {
+    const containers = document.querySelectorAll('.fact-container');
+    // currentBtn = Button.getAttribute('data-button');
+    for (let i = 0; i < containers.length; i++) {
+      let curCont = containers[i].getAttribute('data-container');
+      console.log(curCont);
+    }
   }
 
   render() {
