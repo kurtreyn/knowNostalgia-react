5ade36dbf695163fed92cf76af10d9c90f00757c
kurtreyn <kurtreyn@yahoo.com>
2021-12-30 21:43:10 -0500

    api works. need develop means of separating movies from tv shows

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,13 +1,168 @@
 import React from 'react';
 
 const SearchPage = ({ item }) => {
-  async function loadMovies(searchTitle) {
+  async function loadEm(searchTitle) {
     const url = `https://omdbapi.com/?s=${searchTitle}&page=1&apikey=99e6a288`;
     const searchRes = await fetch(`${url}`);
     const data = await searchRes.json();
     if (data.Response === 'True') console.log(data.Search);
-    // if (data.Response === 'True') displayMovieList(data.Search);
+    if (data.Response === 'True') displayList(data.Search);
   }
+
+  function findEm() {
+    const searchBox = document.querySelector('#title');
+    const searchList = document.getElementById('search-list');
+    let searchTerm = searchBox.value.trim();
+    // console.log(searchTerm);
+    if (searchTerm.length > 0) {
+      searchList.classList.remove('hide-search-list');
+      loadEm(searchTerm);
+    } else {
+      searchList.classList.add('hide-search-list');
+    }
+  }
+
+  function loadDetails() {
+    const searchList = document.getElementById('search-list');
+    const searchBox = document.querySelector('#title');
+    const resultGrid = document.getElementById('result-grid');
+    const searchListItems = searchList.querySelectorAll('.search-list-item');
+    searchListItems.forEach((item) => {
+      // console.log(item);
+      item.addEventListener('click', async () => {
+        // console.log(item.dataset.id);
+        searchList.classList.add('hide-search-list');
+        searchBox.value = '';
+        const resDetails = await fetch(
+          `https://www.omdbapi.com/?i=${item.dataset.id}&apikey=99e6a288`
+        );
+        const movieDetails = await resDetails.json();
+        const movieYear = movieDetails.Year;
+        // console.log(movieYear);
+        if (movieYear >= '1990' && movieYear <= '2009') {
+          displayDetails(movieDetails);
+        } else {
+          resultGrid.innerText = 'Movie was not released between 1990 - 2009';
+        }
+        const tvShowDetails = await resDetails.json();
+        const tvYear = tvShowDetails.Year;
+        // console.log(tvShowDetails);
+        if (tvYear >= '1990' && tvYear <= '2009') {
+          displayDetails(tvShowDetails);
+        } else {
+          resultGrid.innerText = 'TV Show was not released between 1990 - 2009';
+        }
+      });
+    });
+  }
+
+  function displayDetails(details) {
+    const resultGrid = document.getElementById('result-grid');
+    resultGrid.innerHTML = `
+    <div class="movie-poster">
+    <img
+      src="${
+        details.Poster !== 'N/A'
+          ? details.Poster
+          : '../assets/images/image_not_found.png'
+      }"
+      alt="movie poster"
+    />
+  </div>
+  <div class="movie-info">
+    <h3 class="movie-title">${details.Title}</h3>
+    <ul class="movie-misc-info">
+      <li class="year">Year: ${details.Year}</li>
+    </ul>
+    <p class="plot">
+      <b>Plot:</b> ${details.Plot}
+    </p>
+  </div>
+    `;
+  }
+
+  function displayList(things) {
+    const searchList = document.getElementById('search-list');
+    // const resultGrid = document.getElementById('result-grid');
+    let poster;
+    searchList.innerHTML = '';
+    for (let idx = 0; idx < things.length; idx++) {
+      let movieListItem = document.createElement('div');
+      let tvListItem = document.createElement('div');
+      // console.log(movieListItem);
+      movieListItem.dataset.id = things[idx].imdbID;
+      tvListItem.dataset.id = things[idx].imdbID;
+      movieListItem.classList.add('search-list-item');
+      tvListItem.classList.add('search-list-item');
+      if (things[idx].Poster !== 'N/A') {
+        poster = things[idx].Poster;
+      } else {
+        poster = '../assets/images/image_not_found.png';
+      }
+      let movieYear = things[idx].Year;
+      let movieType = things[idx].Type;
+      let tvYear = things[idx].Year;
+      let tvType = things[idx].Type;
+      if (movieYear >= '1990' && movieYear <= '2009' && movieType === 'movie') {
+        movieListItem.innerHTML = `
+        <div class="search-list-item">
+        <div class="search-item-thumbnail">
+          <img src="${poster}" />
+        </div>
+      </div>
+        `;
+        searchList.appendChild(movieListItem);
+      } else {
+        movieListItem.innerHTML = `'../assets/images/image_not_found.png'`;
+      }
+      if (tvYear >= '1990' && tvYear <= '2009' && tvType === 'series') {
+        tvListItem.innerHTML = `
+        <div class="search-list-item">
+        <div class="search-item-thumbnail">
+          <img src="${poster}" />
+        </div>
+      </div>
+        `;
+        searchList.appendChild(tvListItem);
+      } else {
+        tvListItem.innerHTML = '../assets/images/image_not_found.png';
+      }
+    }
+    loadDetails();
+  }
+
+  // function TVdisplayList(things) {
+  //   const searchList = document.getElementById('search-list');
+  //   // const resultGrid = document.getElementById('result-grid');
+  //   let poster;
+  //   searchList.innerHTML = '';
+  //   for (let idx = 0; idx < things.length; idx++) {
+  //     let tvListItem = document.createElement('div');
+  //     // console.log(movieListItem);
+  //     tvListItem.dataset.id = things[idx].imdbID;
+  //     tvListItem.classList.add('search-list-item');
+  //     if (things[idx].Poster !== 'N/A') {
+  //       poster = things[idx].Poster;
+  //     } else {
+  //       poster = '../assets/images/image_not_found.png';
+  //     }
+  //     let tvYear = things[idx].Year;
+  //     let tvType = things[idx].Type;
+  //     if (tvYear >= '1990' && tvYear <= '2009' && tvType === 'series') {
+  //       tvListItem.innerHTML = `
+  //       <div class="search-list-item">
+  //       <div class="search-item-thumbnail">
+  //         <img src="${poster}" />
+  //       </div>
+  //     </div>
+  //       `;
+  //       searchList.appendChild(tvListItem);
+  //     } else {
+  //       tvListItem.innerHTML = '../assets/images/image_not_found.png';
+  //     }
+  //   }
+  //   loadDetails();
+  // }
 
   return (
     <div className="container" key={item.id}>
@@ -22,7 +177,8 @@ const SearchPage = ({ item }) => {
                 className="form-control"
                 id={item.searchID}
                 placeholder={item.placeHolder}
-                onKeyUp={loadMovies}
+                data-type={item.type}
+                onKeyUp={findEm}
               />
             </div>
             {/* <div className="form-group">
