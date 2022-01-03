d46ae31ef785f7b654e93a8788b05359461ec11a
KurtReyn <kurtreyn@yahoo.com>
2022-01-03 10:14:43 -0500

    work on seperating concerns of search api. tv shows is now separate, movie page not displaying results

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,28 +1,40 @@
 import React from 'react';
 
 const SearchPage = ({ item }) => {
-  async function loadEm(searchTitle) {
+  async function loadMovies(searchTitle) {
+    const url = `https://omdbapi.com/?s=${searchTitle}&page=1&apikey=99e6a288`;
+    const searchRes = await fetch(`${url}`);
+    const data = await searchRes.json();
+    if (data.Response === 'True') console.log(data.Search);
+    if (data.Response === 'True') displayMovieList(data.Search);
+  }
+
+  async function loadTVshows(searchTitle) {
     const url = `https://omdbapi.com/?s=${searchTitle}&page=1&apikey=99e6a288`;
     const searchRes = await fetch(`${url}`);
     const data = await searchRes.json();
     // if (data.Response === 'True') console.log(data.Search);
-    if (data.Response === 'True') displayList(data.Search);
+    if (data.Response === 'True') displayTVList(data.Search);
   }
 
   function findEm() {
     const searchBox = document.querySelector('#title');
+    const searchType = searchBox.dataset.type;
     const searchList = document.getElementById('search-list');
     let searchTerm = searchBox.value.trim();
     // console.log(searchTerm);
-    if (searchTerm.length > 0) {
+    if (searchType === 'movie' && searchTerm.Length > 0) {
       searchList.classList.remove('hide-search-list');
-      loadEm(searchTerm);
+      loadMovies(searchTerm);
+    } else if (searchType === 'series' && searchTerm.length > 0) {
+      searchList.classList.remove('hide-search-list');
+      loadTVshows(searchTerm);
     } else {
       searchList.classList.add('hide-search-list');
     }
   }
 
-  function loadDetails() {
+  function loadMovieDetails() {
     const searchList = document.getElementById('search-list');
     const searchBox = document.querySelector('#title');
     const resultGrid = document.getElementById('result-grid');
@@ -44,6 +56,25 @@ const SearchPage = ({ item }) => {
         } else {
           resultGrid.innerText = 'Movie was not released between 1990 - 2009';
         }
+      });
+    });
+  }
+
+  function loadTVDetails() {
+    const searchList = document.getElementById('search-list');
+    const searchBox = document.querySelector('#title');
+    const resultGrid = document.getElementById('result-grid');
+    const searchListItems = searchList.querySelectorAll('.search-list-item');
+    searchListItems.forEach((item) => {
+      // console.log(item);
+      item.addEventListener('click', async () => {
+        // console.log(item.dataset.id);
+        searchList.classList.add('hide-search-list');
+        searchBox.value = '';
+        const resDetails = await fetch(
+          `https://www.omdbapi.com/?i=${item.dataset.id}&apikey=99e6a288`
+        );
+
         const tvShowDetails = await resDetails.json();
         const tvYear = tvShowDetails.Year;
         // console.log(tvShowDetails);
@@ -81,29 +112,23 @@ const SearchPage = ({ item }) => {
     `;
   }
 
-  function displayList(things) {
+  function displayMovieList(things) {
     const searchList = document.getElementById('search-list');
     // const resultGrid = document.getElementById('result-grid');
     let poster;
     searchList.innerHTML = '';
     for (let i = 0; i < things.length; i++) {
       let movieListItem = document.createElement('div');
-      let tvListItem = document.createElement('div');
       // console.log(movieListItem);
       movieListItem.dataset.id = things[i].imdbID;
-      tvListItem.dataset.id = things[i].imdbID;
       movieListItem.classList.add('search-list-item');
-      tvListItem.classList.add('search-list-item');
       if (things[i].Poster !== 'N/A') {
         poster = things[i].Poster;
       } else {
         poster = '../assets/images/image_not_found.png';
       }
-
       let movieYear = things[i].Year;
       let movieType = things[i].Type;
-      let tvYear = things[i].Year;
-      let tvType = things[i].Type;
       if (movieYear >= '1990' && movieYear <= '2009' && movieType === 'movie') {
         movieListItem.innerHTML = `
         <div class="search-list-item">
@@ -116,6 +141,27 @@ const SearchPage = ({ item }) => {
       } else {
         movieListItem.innerHTML = `'../assets/images/image_not_found.png'`;
       }
+    }
+    loadMovieDetails();
+  }
+
+  function displayTVList(things) {
+    const searchList = document.getElementById('search-list');
+    // const resultGrid = document.getElementById('result-grid');
+    let poster;
+    searchList.innerHTML = '';
+    for (let i = 0; i < things.length; i++) {
+      let tvListItem = document.createElement('div');
+      // console.log(movieListItem);
+      tvListItem.dataset.id = things[i].imdbID;
+      tvListItem.classList.add('search-list-item');
+      if (things[i].Poster !== 'N/A') {
+        poster = things[i].Poster;
+      } else {
+        poster = '../assets/images/image_not_found.png';
+      }
+      let tvYear = things[i].Year;
+      let tvType = things[i].Type;
       if (tvYear >= '1990' && tvYear <= '2009' && tvType === 'series') {
         tvListItem.innerHTML = `
         <div class="search-list-item">
@@ -129,48 +175,15 @@ const SearchPage = ({ item }) => {
         tvListItem.innerHTML = '../assets/images/image_not_found.png';
       }
     }
-    loadDetails();
+    loadTVDetails();
   }
-
-  // function TVdisplayList(things) {
-  //   const searchList = document.getElementById('search-list');
-  //   // const resultGrid = document.getElementById('result-grid');
-  //   let poster;
-  //   searchList.innerHTML = '';
-  //   for (let idx = 0; idx < things.length; idx++) {
-  //     let tvListItem = document.createElement('div');
-  //     // console.log(movieListItem);
-  //     tvListItem.dataset.id = things[idx].imdbID;
-  //     tvListItem.classList.add('search-list-item');
-  //     if (things[idx].Poster !== 'N/A') {
-  //       poster = things[idx].Poster;
-  //     } else {
-  //       poster = '../assets/images/image_not_found.png';
-  //     }
-  //     let tvYear = things[idx].Year;
-  //     let tvType = things[idx].Type;
-  //     if (tvYear >= '1990' && tvYear <= '2009' && tvType === 'series') {
-  //       tvListItem.innerHTML = `
-  //       <div class="search-list-item">
-  //       <div class="search-item-thumbnail">
-  //         <img src="${poster}" />
-  //       </div>
-  //     </div>
-  //       `;
-  //       searchList.appendChild(tvListItem);
-  //     } else {
-  //       tvListItem.innerHTML = '../assets/images/image_not_found.png';
-  //     }
-  //   }
-  //   loadDetails();
-  // }
 
   return (
     <div className="container" key={item.id}>
       <div className="row custom-row search-row">
         <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
           <form>
-            <h2>Search Movies</h2>
+            <h2>Search {item.SearchPage}</h2>
             <div className="form-group">
               <label for={item.searhID}></label>
               <input
